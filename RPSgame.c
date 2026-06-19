9a4b46f312c676be16edc7be2e57b062ed36cbb8
sirfranciscofrank <sirfrancisco2003@gmail.com>
2026-06-19 13:39:44 +0700

    Feature: Add greeting message

--- RPSgame.c
+++ RPSgame.c
@@ -2,6 +2,10 @@
 
 //Create: 1.) List of Choices 2. Single Char choices 3. Score Calculator
 int main(void){
+
+//start of with User Greeting.
+	printf("Welcome to  Classic Rock Paper Scissors");
+//Create game choices or input for further game logic
 	char *chocies[3]= {"Rock" , "Paper" , "Scissors"};
 
 
